54ba1be749decc4202c7ef6b194ca83e2a9eb3f7
asdasd <sdfsdfsds@gmail.com>
2024-12-11 09:25:01 +0100

    users, products, cart

--- Webshop_MegaProjekt_MPA/backend/src/app.module.ts
+++ Webshop_MegaProjekt_MPA/backend/src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { CartModule } from './cart/cart.module';
+import { ProductsModule } from './products/products.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, ProductsModule, CartModule],
   controllers: [AppController],
   providers: [AppService],
 })
