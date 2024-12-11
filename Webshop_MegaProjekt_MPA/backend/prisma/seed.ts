6e399bb7a8423b1bb773636b7d2720f7551f5cb2
asdasd <sdfsdfsds@gmail.com>
2024-12-11 11:31:32 +0100

    seed.ts, + material added

--- Webshop_MegaProjekt_MPA/backend/prisma/seed.ts
+++ Webshop_MegaProjekt_MPA/backend/prisma/seed.ts
@@ -3,3 +3,26 @@ import { PrismaClient } from "@prisma/client";
 import { create } from 'domain';
 const prisma = new PrismaClient()
 
+async function main() {
+    for (let i = 0; i < 100000; i++) {
+        await prisma.product.create({
+          data: {
+            name        : faker.commerce.productName(),
+            imageUrl    : faker.image.urlPicsumPhotos(),
+            material    : faker.commerce.productMaterial(),
+            description : faker.commerce.productDescription(),
+            price       : parseFloat(faker.commerce.price()),
+          },
+        });
+      };
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    //process.exit(1)
+  })
