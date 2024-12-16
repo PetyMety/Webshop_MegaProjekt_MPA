e6b17fc27b0ce9d42107cf044f237c8d7df9e567
asdasd <sdfsdfsds@gmail.com>
2024-12-16 08:36:27 +0100

    schema user-rel kiegészítve, auth resource léttrehozása

--- Webshop_MegaProjekt_MPA/backend/src/app.module.ts
+++ Webshop_MegaProjekt_MPA/backend/src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 import { CartModule } from './cart/cart.module';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule, CartModule],
+  imports: [UsersModule, ProductsModule, CartModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
