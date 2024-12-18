a7b7eebae25e07662a852a49a756351214c9b586
Peti <nemt451@gmail.com>
2024-12-18 09:48:46 +0100

    auth, product kész

--- Webshop_MegaProjekt_MPA/backend/src/auth/auth.module.ts
+++ Webshop_MegaProjekt_MPA/backend/src/auth/auth.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { TokenStrategy } from './token.strategy';
+import { UsersModule } from 'src/users/users.module';
+
 
 @Module({
+  imports: [UsersModule],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, TokenStrategy],
 })
 export class AuthModule {}
--- Webshop_MegaProjekt_MPA/backend/src/products/products.service.ts
+++ Webshop_MegaProjekt_MPA/backend/src/products/products.service.ts
@@ -1,19 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class ProductsService {
+
+  constructor(private readonly prisma: PrismaService){}
+
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.prisma.product.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product`;
+    return this.prisma.product.findUnique({ where : {id}})
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
